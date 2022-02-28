915aaa371659e0f339473e982a49708a9227d7b5
yamatsum <sy12250@gmail.com>
2022-03-01 00:14:38 +0900

    BREAKING CHANGE: rewriting using only lua.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # nvim-cursorline
 
-Highlight words and lines on the cursor for Neovim 
+Highlight words and lines on the cursor for Neovim
 
 - Underlines the word under the cursor.
 - Show / hide cursorline in connection with cursor moving.
@@ -8,18 +8,27 @@ Highlight words and lines on the cursor for Neovim
 ![demo](https://user-images.githubusercontent.com/42740055/102508634-f4d26c80-40c8-11eb-90af-142a7a63837d.gif)
 
 ## Installation
+
 Install with your favorite plugin manager.
 
-## Highlighting
-You can override cursor highlighting by defining `CursorWord` group and disabling built-in highlighting by specifying `vim.g.cursorword_highlight` (lua) or `g:cursorword_highlight` (vimscript).
+## Usage
 
-To lower the time it takes to make the cursorline appear, set `vim.g.cursorline_timeout` (lua) or `g:cursorline_timeout` (vimscript), where `1000` is the default value in milliseconds.
+```lua
+require('nvim-cursorline').setup {
+  cursorline = {
+    enable = true
+    timeout = 1000
+  }
+}
+```
 
 ## Acknowledgments
+
 Thanks goes to these people/projects for inspiration:
 
 - [delphinus/vim-auto-cursorline](https://github.com/delphinus/vim-auto-cursorline)
 - [itchyny/vim-cursorword](https://github.com/itchyny/vim-cursorword)
 
 ## License
+
 This software is released under the MIT License, see LICENSE.
--- lua/nvim-cursorline.lua
+++ lua/nvim-cursorline.lua
@@ -1,101 +1,52 @@
 local M = {}
 
-local DISABLED = 0
-local CURSOR = 1
-local WINDOW = 2
-local status = CURSOR
+local wo = vim.wo
+local autocmd = vim.api.nvim_create_autocmd
 local timer = vim.loop.new_timer()
 
-local w = vim.w
-local o = vim.o
-local g = vim.g
-local fn = vim.fn
-local api = vim.api
+local DEFAULT_OPTIONS = {
+  cursorline = {
+    enable = true,
+    timeout = 1000,
+  },
+}
 
-local cursorline_timeout = g.cursorline_timeout and g.cursorline_timeout or 1000
+function M.setup(options)
+  M.options = vim.tbl_deep_extend("force", DEFAULT_OPTIONS, options or {})
 
-o.cursorline = true
-
-local function return_highlight_term(group, term)
-  local output = api.nvim_exec("highlight " .. group, true)
-  local hi = fn.matchstr(output, term .. [[=\zs\S*]])
-  if hi == nil or hi == "" then
-    return "None"
-  else
-    return hi
+  if M.options.cursorline.enable then
+    wo.cursorline = true
+    autocmd({
+      event = { "WinEnter" },
+      callback = function()
+        wo.cursorline = true
+      end,
+      once = false,
+    })
+    autocmd({
+      event = { "WinLeave" },
+      callback = function()
+        wo.cursorline = false
+      end,
+      once = false,
+    })
+    autocmd({
+      event = { "CursorMoved", "CursorMovedI" },
+      callback = function()
+        wo.cursorlineopt = "number"
+        timer:start(
+          M.options.cursorline.timeout,
+          0,
+          vim.schedule_wrap(function()
+            wo.cursorlineopt = "both"
+          end)
+        )
+      end,
+      once = false,
+    })
   end
 end
 
-local normal_bg = return_highlight_term("Normal", "guibg")
-local cursorline_bg = return_highlight_term("CursorLine", "guibg")
-
-function M.highlight_cursorword()
-  if g.cursorword_highlight ~= false then
-    vim.cmd("highlight CursorWord term=underline cterm=underline gui=underline")
-  end
-end
-
-function M.matchadd()
-  if fn.hlexists("CursorWord") == 0 then
-    return
-  end
-  local column = api.nvim_win_get_cursor(0)[2]
-  local line = api.nvim_get_current_line()
-  local cursorword = fn.matchstr(line:sub(1, column + 1), [[\k*$]])
-    .. fn.matchstr(line:sub(column + 1), [[^\k*]]):sub(2)
-
-  if cursorword == w.cursorword then
-    return
-  end
-  w.cursorword = cursorword
-  if w.cursorword_match == 1 then
-    vim.call("matchdelete", w.cursorword_id)
-  end
-  w.cursorword_match = 0
-  if cursorword == "" or #cursorword > 100 or #cursorword < 3 or string.find(cursorword, "[\192-\255]+") ~= nil then
-    return
-  end
-  local pattern = [[\<]] .. cursorword .. [[\>]]
-  w.cursorword_id = fn.matchadd("CursorWord", pattern, -1)
-  w.cursorword_match = 1
-end
-
-function M.cursor_moved()
-  M.matchadd()
-  if status == WINDOW then
-    status = CURSOR
-    return
-  end
-  M.timer_start()
-  if status == CURSOR and cursorline_timeout ~= 0 then
-    -- disable cursorline
-    vim.cmd("highlight! CursorLine guibg=" .. normal_bg)
-    vim.cmd("highlight! CursorLineNr guibg=" .. normal_bg)
-    status = DISABLED
-  end
-end
-
-function M.win_enter()
-  o.cursorline = true
-  status = WINDOW
-end
-
-function M.win_leave()
-  o.cursorline = false
-  status = WINDOW
-end
-
-function M.timer_start()
-  timer:start(
-    cursorline_timeout,
-    0,
-    vim.schedule_wrap(function()
-      -- enable cursorline
-      vim.cmd("highlight! CursorLine guibg=" .. cursorline_bg)
-      vim.cmd("highlight! CursorLineNr guibg=" .. cursorline_bg)
-      status = CURSOR
-    end)
-  )
-end
+M.options = nil
 
 return M
